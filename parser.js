627457738bbf7624b366a184af9990d86324a6f9
DarthVanger <darthvanger@gmail.com>
2020-05-30 20:37:10 +0300

    Output JS code for the command :)

--- parser.js
+++ parser.js
@@ -2,59 +2,66 @@ import querySelectors from './querySelectors.js';
 import domEvents from './domEvents.js';
 import commands from './commands.js';
 const editor = document.querySelector('#freecode-editor');
+const jsOutput = document.querySelector('#js-output');
 
 let text;
+let highlightedEditorText;
 function parse(textToParse) {
   text = textToParse;
+  highlightedEditorText = text;;
   const expressions = categorizeExpressions(text);
   console.log('Parsed expressions: ', expressions);
 
-  let highlightedEditorText = text;
   expressions.forEach(hightlightExpressionInEditor);
-  function hightlightExpressionInEditor(e) {
-    const expressionIndex = highlightedEditorText.indexOf(e.text);
-    const expressionLength = e.text.length;
-    const backgroundColors = {
-      querySelector: '#007bff',
-      domEvent: '#28a745' ,
-      command: '#ffc107',
-    };
-
-    const bgColor = backgroundColors[e.type];
-
-    highlightedEditorText = `
-      ${highlightedEditorText.substring(0, expressionIndex)}
-      <span style="background-color: ${bgColor}" class="wrapper"/>
-        ${e.text}
-        <span class="tooltip" style="background-color: ${bgColor}; border-top-color: ${bgColor}">${e.type}</span>
-      </span>
-      ${highlightedEditorText.substring(expressionIndex + expressionLength)}
-    `;
-
-    highlightedEditorText = highlightedEditorText.split('\n').map(l => l.replace(/\s+[<]/g, '<')).join('');
-
-    editor.innerHTML = highlightedEditorText;
-  }
-
   expressions.forEach(e => e.type === 'command' && executeCommand(e));
-    
 };
+
+function hightlightExpressionInEditor(e) {
+  const expressionIndex = highlightedEditorText.indexOf(e.text);
+  const expressionLength = e.text.length;
+  const backgroundColors = {
+    querySelector: '#007bff',
+    domEvent: '#28a745' ,
+    command: '#ffc107',
+  };
+
+  const bgColor = backgroundColors[e.type];
+
+  highlightedEditorText = `
+    ${highlightedEditorText.substring(0, expressionIndex)}
+    <span style="background-color: ${bgColor}" class="wrapper"/>
+      ${e.text}
+      <span class="tooltip" style="background-color: ${bgColor}; border-top-color: ${bgColor}">${e.type}</span>
+    </span>
+    ${highlightedEditorText.substring(expressionIndex + expressionLength)}
+  `;
+
+  highlightedEditorText = highlightedEditorText.split('\n').map(l => l.replace(/\s+[<]/g, '<')).join('');
+
+  editor.innerHTML = highlightedEditorText;
+}
+    
 
 function executeCommand (expression) {
   const commandName = expression.text;
   console.log('Command to execute: ', commandName);
 
   // finds commands['show text'] = (text) => document.print(text)
-  const commandJsFunction = commands[commandName];
-  console.log('commandJsFunction: ', commandJsFunction.toString());
+  const commandJsFunctionText = commands[commandName].toString();
+  console.log('commandJsFunction: ', commandJsFunctionText);
 
   const commandArgument = findCommandArgument({ expression });
   console.log('commandArgument: ', commandArgument);
-  const executeCurrentCommand = () => commandJsFunction(commandArgument);
 
-  console.log('executeCurrentCommand: ', executeCurrentCommand.toString());
+  const code = `const ${commandName.replace(' ', '')} = ${commandJsFunctionText}`;
+  console.log('code: ', code);
 
-  executeCurrentCommand();
+  jsOutput.innerText = jsOutput.innerText + code;
+  //const executeCurrentCommand = () => commandJsFunction(commandArgument);
+
+  //console.log('executeCurrentCommand: ', executeCurrentCommand.toString());
+
+  //executeCurrentCommand();
 
 }
 
