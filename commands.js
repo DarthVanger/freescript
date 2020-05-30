74a6ecc10e3f5c81499621d3cdefe51c485a6619
DarthVanger <darthvanger@gmail.com>
2020-05-30 22:38:20 +0300

    Revert simplifying 'show text' command :)

--- commands.js
+++ commands.js
@@ -1,6 +1,8 @@
 const commands = {
   'show text': (text) => {
-    window.alert(text);
+    const div = document.createElement('div');
+    div.innerHTML = text;
+    document.body.appendChild(div);
   }
 };
 
